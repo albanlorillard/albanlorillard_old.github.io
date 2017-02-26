b20d2fd7efa85e840f80d85fcc1c70e22267d2a2
Alban LORILLARD <alban.lorillard@etu.univ-nantes.fr>
2017-02-26 10:59:58 +0100

    init Jekyll

--- routes/index_en.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Alban Lorillard personnal website'});
-});
-
-module.exports = router;
